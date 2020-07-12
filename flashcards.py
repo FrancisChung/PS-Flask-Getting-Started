d79ecf059395bf7b01f6c40ff39437f51d09a9b5
Francis Chung <francis.chung@bondexchange.com.au>
2020-07-12 10:58:33 +1000

    using render_template

--- flashcards.py
+++ flashcards.py
@@ -1,18 +1,9 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
-welcomeView = 0
-dateView = 0
+
 
 @app.route("/")
 def welcome():
-    return "Welcome to my flash application!"
-
-
-@app.route("/date")
-def date():
-    global dateView
-    dateView += 1
-    return "This page was served at " + str(datetime.now()) + " and viewed " + str(dateView) + " times"
+    return render_template("welcome.html")
