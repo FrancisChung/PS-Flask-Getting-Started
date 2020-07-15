7391ab1089f7522f552a0c5bbbb4ec8e5f1cccba
Francis Chung <francis.chung@bondexchange.com.au>
2020-07-15 22:05:08 +1000

    New Add_Card route

--- flashcards.py
+++ flashcards.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, abort, jsonify
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
 from model import db
-
 
 app = Flask(__name__)
 
@@ -14,9 +13,23 @@ def welcome():
 def card_view(index):
     try:
         card = db[index]
-        return render_template("card.html", card=card, index=index, max_index=len(db)-1)
+        return render_template("card.html", card=card, index=index, max_index=len(db) - 1)
     except IndexError:
         abort(404)
+
+
+@app.route("/add_card", methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        card = {
+            "question": request.form['question'],
+            "answer": request.form['answer']
+        }
+        db.append(card)
+        return redirect(url_for("card_view", index=len(db) - 1))
+    else:
+        return render_template("add_card.html")
+
 
 @app.route("/api/card")
 def api_card_list():
@@ -29,4 +42,3 @@ def api_card_detail(index):
         return db[index]
     except IndexError:
         abort(404)
-
