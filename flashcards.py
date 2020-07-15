f55b37dfc2476e06ce53e076702e161c21707fb7
Francis Chung <francis.chung@bondexchange.com.au>
2020-07-16 08:52:57 +1000

    Save_Db()

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
-from model import db
+from model import db, save_db
 
 app = Flask(__name__)
 
@@ -26,6 +26,7 @@ def add_card():
             "answer": request.form['answer']
         }
         db.append(card)
+        save_db()
         return redirect(url_for("card_view", index=len(db) - 1))
     else:
         return render_template("add_card.html")
